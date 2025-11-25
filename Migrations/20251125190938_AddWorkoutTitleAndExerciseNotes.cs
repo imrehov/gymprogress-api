bc64c0288ea6b05bd0178beac7bea736c741c5d8
imrehov <114602851+imrehov@users.noreply.github.com>
2025-11-25 20:09:56 +0100

    db migrate

---
+++ Migrations/20251125190938_AddWorkoutTitleAndExerciseNotes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymProgressTrackerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkoutTitleAndExerciseNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Workouts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExerciseNotes",
+                table: "Exercises",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Workouts");
+
+            migrationBuilder.DropColumn(
+                name: "ExerciseNotes",
+                table: "Exercises");
+        }
+    }
+}
